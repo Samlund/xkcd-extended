c6083065dad497632ec15ecffc39138bbcfac83d
anotona <129624524+notonanotonanoton@users.noreply.github.com>
2026-06-02 15:17:14 +0200

    fix: remove redundant navbar from favorites

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import { useQuery } from "@tanstack/react-query";
 import { xkcd } from "@/lib/external/xkcd";
-import Navbar from "@/components/layout/Navbar";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Toggle } from "@/components/ui/toggle";
@@ -84,7 +83,6 @@ function Favorites() {
     if (favoritesQuery.isPending) {
         return (
             <div className="flex flex-col min-h-screen">
-                <Navbar />
                 <div className="flex justify-center items-center flex-grow">
                     <Spinner className="size-8" />
                 </div>
